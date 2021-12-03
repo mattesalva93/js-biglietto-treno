e8e35ce268fa230f8eb3e144aea5c7f0ce91bd9d
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-03 15:19:26 +0100

    prompt and ticketprice done

---
+++ myscript.js
@@ -0,0 +1,10 @@
+const inputdistance = prompt("Inserisci la distanza del tragitto (Km)")
+
+const inputage = prompt("Inserisci la tua età")
+
+console.log(inputdistance)
+console.log(inputage)
+
+let ticketprice = (inputdistance * 0.21)
+
+console.log (ticketprice)
